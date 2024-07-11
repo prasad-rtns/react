2383a4569eb735e372f7c8659770f1209d6c2bbd
for-each-ref --sort='authordate:iso8601' --format='%(color:green)%(authordate:relative)%09%(color:reset)%(refname:short)' refs/heads <pn00629050@techmahindra.com>
2024-07-12 03:38:00 +0530

    React 11-July-2024

---
+++ message.js
@@ -0,0 +1,9 @@
+import { name, age } from "./person.js";
+
+const message = () => {
+    const name = "Jesse";
+    const age = 40;
+    return name + ' is ' + age + 'years old.';
+};
+  
+export default message;
